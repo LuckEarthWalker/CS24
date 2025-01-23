22e8a41815575af962c3db1870bec38597c6ad98
Jay <yiliang0604@gmail.com>
2025-01-22 17:56:00 -0800

    attempt 1 at tictac lab

--- labs/tictac/Move.cpp
+++ labs/tictac/Move.cpp
@@ -1,4 +1,43 @@
 #include "Errors.h"
 #include "Move.h"
+#include <sstream>
+#include <iostream>
 
-// Space for implementing Move functions.
+Move::Move(const std::string& input) {
+
+    std::istringstream move_ln(input);
+    
+    //check move number
+    int temp;
+    move_ln >> temp;
+    if (temp < 1 || temp > 9) {
+        std::cerr << "Parse error.\n";
+        exit(1);
+    }
+    number = temp;
+
+    //check player code
+    std::string buffer;
+    move_ln >> buffer;
+    if (buffer.size() != 1 || (toupper(buffer[0]) != 'X' && toupper(buffer[0]) != 'O')) {
+        std::cerr << "Parse error.\n";
+        exit(1);
+    }
+    player = toupper(buffer[0]);
+
+    //check square code
+    move_ln >> buffer;
+    buffer[0] = toupper(buffer[0]);
+    if (buffer.size() != 2 || (buffer[0] != 'A' && buffer[0] != 'B' && buffer[0] != 'C') || (buffer[1] != '1' && buffer[1] != '2' && buffer[1] != '3')) {
+        std::cerr << "Parse error.\n";
+        exit(1);
+    }
+    row = buffer[0] - 64;
+    column = buffer[1] - '0';
+}
+
+std::string Move::to_string() const {
+    std::string str(1, player);
+    std::string str1(1, '@'+row);
+    return std::to_string(number) + " " + str + " " + str1 + std::to_string(column);
+}
