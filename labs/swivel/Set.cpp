659861a21198391c520d508050da01d1925be1f3
Jay <yiliang0604@gmail.com>
2025-02-12 23:44:19 -0800

    added remove and swivel

--- labs/swivel/Set.cpp
+++ labs/swivel/Set.cpp
@@ -181,10 +181,106 @@ void Set::print() const {
     std::cout << "\n";
 }
 
-size_t Set::remove(const std::string& value) {
-    return 1;
+static Node* get_parent(Node* root, Node* child) {
+    if (root == child)
+        return NULL;
+
+    Node* parent = NULL;
+    Node* current = root;
+    while (current != NULL && current != child) {
+        parent = current;
+        if (child->data < current->data)
+            current = current->left;
+        else
+            current = current->right;
+    }
+    return parent;
 }
 
 bool Set::swivel(const std::string& value) {
-    return true;
+    if (mRoot == NULL)
+        return false;
+
+    Node* current = mRoot;
+    Node* last = mRoot;
+
+    while (current != NULL) {
+        last = current;
+        if (value == current->data)
+            break;
+        else if (value < current->data)
+            current = current->left;
+        else
+            current = current->right;
+    }
+
+    if (current == NULL)
+        current = last;
+    bool found = (current->data == value);
+
+    while (mRoot != current) {
+        Node* parent = get_parent(mRoot, current);
+        if (parent == NULL)
+            break;  
+
+        if (parent->left == current) {
+            // Right rotation.
+            parent->left = current->right;
+            current->right = parent;
+        } else {
+            // Left rotation.
+            parent->right = current->left;
+            current->left = parent;
+        }
+
+        Node* grandparent = get_parent(mRoot, parent);
+        if (grandparent == NULL)
+            mRoot = current;
+        else {
+            if (grandparent->left == parent)
+                grandparent->left = current;
+            else
+                grandparent->right = current;
+        }
+    }
+    return found;
+}
+
+size_t Set::remove(const std::string& value) {
+    if (mRoot == NULL)
+        return 0;
+
+    bool found = swivel(value);
+    if (!found)
+        return 0;  
+
+    Node* toDelete = mRoot;
+
+    // at most one child.
+    if (mRoot->left == NULL || mRoot->right == NULL) {
+        mRoot = (mRoot->left != NULL) ? mRoot->left : mRoot->right;
+        delete toDelete;
+        mCount--;
+        return 1;
+    }
+
+    // two children.
+    Node* leftSubtree = mRoot->left;
+    
+    // Promote the right subtree.
+    mRoot = mRoot->right;
+    delete toDelete;
+    mCount--;
+
+    // find smallest node in new tree
+    Node* smallest = mRoot;
+    while (smallest->left != NULL)
+        smallest = smallest->left;
+
+    // smallest node -> new root.
+    swivel(smallest->data);
+
+    mRoot->left = leftSubtree;
+
+    return 1;
 }
