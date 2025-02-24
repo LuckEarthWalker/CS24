9a2e77df220d4ede1cb25a74fe27195a3f31c74f
Jay <yiliang0604@gmail.com>
2025-02-24 00:39:23 -0800

    added the wordlist constructor

--- labs/typo/WordList.cpp
+++ labs/typo/WordList.cpp
@@ -1,7 +1,17 @@
 #include "WordList.h"
 
 WordList::WordList(std::istream& stream) {
-
+    std::string buffer;
+    while (getline(stream, buffer)) {
+        for (size_t i = 0; i < buffer.length(); i++) {
+            if (isspace(buffer[i])) {
+                buffer.erase(i,1);
+            } else if (!(isalpha(buffer[i]) && islower(buffer[i]))) {
+                break;
+            }
+        }
+        mWords.push_back(buffer);
+    }
 }
 
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
