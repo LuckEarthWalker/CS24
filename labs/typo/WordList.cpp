ad4b73299997c290def970502c86f67f2328a8d9
Jay <yiliang0604@gmail.com>
2025-02-24 01:03:43 -0800

    added correct to WordList

--- labs/typo/WordList.cpp
+++ labs/typo/WordList.cpp
@@ -1,4 +1,5 @@
 #include "WordList.h"
+#include <cmath>
 
 WordList::WordList(std::istream& stream) {
     std::string buffer;
@@ -15,6 +16,20 @@ WordList::WordList(std::istream& stream) {
 }
 
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
+    Heap candidates(maxcount);
+    for (std::string buffer : mWords) {
+        if (buffer.length() == points.size()) {
+            double s_sum = 0;
+            for (size_t i = 0; i < points.size(); i++) {
+                double d = pow(pow(points[i].x-QWERTY[buffer[i]-'a'].x, 2) + pow(points[i].y-QWERTY[buffer[i]-'a'].y, 2), .5); //pythagoran theorem
+                s_sum += 1/(10*pow(d,2)+1);
+            }
+            candidates.push(buffer, (float)(s_sum/points.size()));
+        }
+    }
+    while (candidates.top().score < cutoff) {
+        candidates.pop();
+    }
     Heap stub(3);
     return stub;
 }
