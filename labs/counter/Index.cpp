3120882234a130cdd79462c72a9d80fc9e8681f9
Jay <yiliang0604@gmail.com>
2025-03-04 02:32:16 -0800

    fixed rehash

--- labs/counter/Index.cpp
+++ labs/counter/Index.cpp
@@ -13,19 +13,20 @@ size_t Index::hashfunc(std::string str) const {
 void Index::rehash() {
     Node** temp = map;
     map = new Node*[map_size*2]();
-    for (size_t i = 0; i < map_size; i++) {
+    size_t previous_msize = map_size;
+    map_size *= 2;
+    for (size_t i = 0; i < previous_msize; i++) {
         if (temp[i] != nullptr) {
             add_key(temp[i]->str, temp[i]);
         }
     }
-    map_size *= 2;
     delete []temp;
     temp = nullptr;
 }
 
 Index::Index() {
-    map = new Node*[32](); // use 32 due to it being closest 2^n to alphabet size
-    map_size = 32;
+    map = new Node*[64](); // use 64 to avoid rehash for char count
+    map_size = 64;
     count = 0;
 }
 
@@ -39,7 +40,6 @@ void Index::add_key(const std::string& key, Node* pair) {
     size_t buffer = index;
 
     while (map[buffer] != nullptr) {
-        buffer = index;
         buffer = (size_t)(index + (offset + pow(offset, 2))/2)%map_size;
         offset++;
     }
@@ -59,8 +59,7 @@ Node* Index::lookup(const std::string& key) const {
     size_t offset = 1;
     size_t buffer = index;
     while (map[buffer] != nullptr && map[buffer]->str != key) {
-        buffer = index;
-        buffer = (size_t)(index + (offset + pow(offset, 2))/2)%map_size;
+        buffer = (size_t)(index + ((double)offset + pow(offset, 2))/2)%map_size;
         offset++;
     }
 
@@ -75,7 +74,6 @@ Node* Index::del_key(const std::string& key) {
     size_t offset = 1;
     size_t buffer = index;
     while (map[buffer] != nullptr && map[buffer]->str != key) {
-        buffer = index;
         buffer = (size_t)(index + (offset + pow(offset, 2))/2)%map_size;
         offset++;
     }
